0f1596bfb0407dcfb974a61b77722635ab4bfb22
Sy C <76875297+SyreetaC@users.noreply.github.com>
2021-03-22 20:31:08 +0000

    construct game container fn, countdown fn

--- js/script.js
+++ js/script.js
@@ -1,13 +1,56 @@
 const startButtonElement = document.getElementById("start-button");
+const bodyElement = document.body;
+const startGameDiv = document.getElementById("start-page-div");
+const timerElement = document.getElementById("time-remaining");
+
+let timerValue = 60;
+
+const countdown = () => {
+  const timerTick = () => {
+    timerValue -= 1;
+    timerElement.textContent = timerValue;
+
+    if (timerValue === 0) {
+      clearInterval(timer);
+    }
+  };
+  const timer = setInterval(timerTick, 1000);
+};
+//Constructing game elements
+const constructGameContainer = () => {
+  const gamePageDiv = document.createElement("div");
+  gamePageDiv.setAttribute("class", "game-page-section");
+
+  const gamePageHeader = document.createElement("h4");
+  gamePageHeader.setAttribute("class", "question-header");
+
+  const gamePageAnswersDiv = document.createElement("div");
+  gamePageAnswersDiv.setAttribute("class", "answer-buttons");
+
+  const gamePageAnswerButton1 = document.createElement("button");
+  gamePageAnswerButton1.setAttribute("class", "answer1");
+
+  gamePageDiv.appendChild(
+    gamePageHeader,
+    gamePageAnswersDiv,
+    gamePageAnswerButton1
+  );
+
+  //add buttons
+  return gamePageDiv;
+};
+
+//Start of game
 const startGame = () => {
-  console.log("Hello");
+  console.log("START GAME");
+  const gamePageDiv = constructGameContainer();
+  bodyElement.removeChild(startGameDiv); //Removes start page
+  bodyElement.appendChild(gamePageDiv); //fix CSS for this appended child!
+
+  countdown();
 };
 
 startButtonElement.addEventListener("click", startGame);
-
-// const startGameDiv = document.querySelector("#start-page-div");
-// const timerElement = document.querySelector("#time-remaining");
-// const bodyElement = document.body;
 
 // let timerValue = 60;
 
