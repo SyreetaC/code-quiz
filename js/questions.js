e3741a9f0a88242d72b74a1ded000b2279a03412
Sy C <76875297+SyreetaC@users.noreply.github.com>
2021-03-22 21:05:12 +0000

    Added quiz questions

---
+++ js/questions.js
@@ -0,0 +1,50 @@
+const quizQuestions = [
+  {
+    question: "Which HTML element do we put the Javascript inside?",
+    answers: {
+      a: "js",
+      b: "script",
+      c: "javascript",
+    },
+    correctAnswer: "b",
+  },
+  {
+    question: "How do you add a comment in Javascript?",
+    answers: {
+      a: "//"
+      b: "<!--->",
+      c: "This is a comment",
+    },
+    correctAnswer: "a",
+  },
+  {
+    question: "Which one of these is a Javascript key word?",
+    answers: {
+      a: "const",
+      b: "lot",
+      c: "create",
+      d: "proof",
+    },
+    correctAnswer: "a",
+  },
+  {
+    question: "What statement supplies the value of a function?",
+    answers: {
+      a: "track",
+      b: "return",
+      c: "let",
+      d: "value",
+    },
+    correctAnswer: "b",
+  },
+  {
+    question: "Why do we use the console when coding in Javascript?",
+    answers: {
+      a: "To make sure everything works",
+      b: "To make our code look good",
+      c: "To test the code we have written",
+      d: "To show the value of variables",
+    },
+    correctAnswer: "c",
+  },
+];
