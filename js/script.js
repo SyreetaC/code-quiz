2260b9db2ea50b4b535a039559f6e3de3ed4ff11
Sy C <76875297+SyreetaC@users.noreply.github.com>
2021-03-24 19:27:29 +0000

    added create questions and choices fn

--- js/script.js
+++ js/script.js
@@ -2,25 +2,25 @@ const quizQuestions = [
   {
     title: "Which HTML element do we put the Javascript inside?",
     answers: ["js", "script", "javascript"],
-    correctAnswer: "script",
+    correctAnswer: 1,
   },
 
   {
     title: "How do you add a comment in Javascript?",
     answers: ["//", "<!--->", "This is a comment"],
-    correctAnswer: "//",
+    correctAnswer: 0,
   },
 
   {
     title: "Which one of these is a Javascript key word?",
     answers: ["const", "lot", "create", "proof"],
-    correctAnswer: "const",
+    correctAnswer: 0,
   },
 
   {
     title: "What statement supplies the value of a function?",
     answers: ["track", "return", "let", "value"],
-    correctAnswer: "return",
+    correctAnswer: 1,
   },
 
   {
@@ -31,7 +31,7 @@ const quizQuestions = [
       "To test the code we have written",
       "To show the value of variables",
     ],
-    correctAnswer: "To test the code we have written",
+    correctAnswer: 2,
   },
 ];
 
@@ -53,38 +53,51 @@ const countdown = () => {
   };
   const timer = setInterval(timerTick, 1000);
 };
+const createChoices = (answers) => {
+  const choiceContainer = document.createElement("div");
 
-const createQuestion = (question) => {};
+  const createChoice = (answer) => {
+    const answersDiv = document.createElement("div");
+    const answerButtons = document.createElement("button");
+    answerButtons.setAttribute("data-answer", answer);
+    answerButtons.textContent = answer;
+
+    answersDiv.appendChild(answerButtons);
+    choiceContainer.appendChild(answersDiv); //appends each choice button to a container.
+
+    //Sets the number of buttons to the number of answers in the button elements.
+  };
+  answers.forEach(createChoice);
+  console.log(answersDiv); //comes up undefined- why?
+};
+
+const createQuestion = (question) => {
+  // console.log(question);
+  const questionContainer = document.createElement("div");
+  questionContainer.setAttribute("id", "question");
+  questionContainer.setAttribute("data-answer", question.correctAnswer);
+
+  const h2 = document.createElement("h2");
+  h2.textContent = question.title;
+
+  questionContainer.appendChild(h2);
+
+  const choices = createChoices(question.answers);
+};
 
 //Constructing game elements
 const constructGameContainer = () => {
   const gamePageDiv = document.createElement("div");
-  gamePageDiv.setAttribute("class", "game-page-section");
+  gamePageDiv.setAttribute("class", "game-page-div");
 
-  const gamePageHeader = document.createElement("h4");
-  gamePageHeader.setAttribute("class", "question-header");
-  gamePageHeader.textContent = "Question 1";
-
-  const gamePageAnswersDiv = document.createElement("div");
-  gamePageAnswersDiv.setAttribute("class", "answer-buttons");
-
-  const gamePageAnswerButton1 = document.createElement("button");
-  gamePageAnswerButton1.setAttribute("class", "answer1");
-
-  gamePageDiv.appendChild(
-    gamePageHeader,
-    gamePageAnswersDiv,
-    gamePageAnswerButton1
-  );
-
-  //add buttons
+  gamePageDiv.appendChild(questionContainer); //Tried to append question container to main game page div.
   return gamePageDiv;
 };
 
 const startButtonElement = document.getElementById("start-button");
 //Start of game
 const startGame = () => {
-  console.log("START GAME");
+  // console.log(constructGameContainer);
   const gamePageDiv = constructGameContainer();
   bodyElement.removeChild(startGameDiv); //Removes start page
   bodyElement.appendChild(gamePageDiv); //fix CSS for this appended child!
