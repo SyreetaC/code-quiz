f53e24cea0ed0f538aaef6389987f45d60f8f4ef
Sy C <76875297+SyreetaC@users.noreply.github.com>
2021-03-24 18:28:05 +0000

    turned questions array into objects

--- js/questions.js
+++ js/questions.js
@@ -1,50 +1,36 @@
 const quizQuestions = [
   {
-    question: "Which HTML element do we put the Javascript inside?",
-    answers: {
-      a: "js",
-      b: "script",
-      c: "javascript",
-    },
-    correctAnswer: "b",
+    title: "Which HTML element do we put the Javascript inside?",
+    answers: ["js", "script", "javascript"],
+    correctAnswer: "script",
   },
+
   {
-    question: "How do you add a comment in Javascript?",
-    answers: {
-      a: "//"
-      b: "<!--->",
-      c: "This is a comment",
-    },
-    correctAnswer: "a",
+    title: "How do you add a comment in Javascript?",
+    answers: ["//", "<!--->", "This is a comment"],
+    correctAnswer: "//",
   },
+
   {
-    question: "Which one of these is a Javascript key word?",
-    answers: {
-      a: "const",
-      b: "lot",
-      c: "create",
-      d: "proof",
-    },
-    correctAnswer: "a",
+    title: "Which one of these is a Javascript key word?",
+    answers: ["const", "lot", "create", "proof"],
+    correctAnswer: "const",
   },
+
   {
-    question: "What statement supplies the value of a function?",
-    answers: {
-      a: "track",
-      b: "return",
-      c: "let",
-      d: "value",
-    },
-    correctAnswer: "b",
+    title: "What statement supplies the value of a function?",
+    answers: ["track", "return", "let", "value"],
+    correctAnswer: "return",
   },
+
   {
-    question: "Why do we use the console when coding in Javascript?",
-    answers: {
-      a: "To make sure everything works",
-      b: "To make our code look good",
-      c: "To test the code we have written",
-      d: "To show the value of variables",
-    },
-    correctAnswer: "c",
+    title: "Why do we use the console when coding in Javascript?",
+    answers: [
+      "To make sure everything works",
+      "To make our code look good",
+      "To test the code we have written",
+      "To show the value of variables",
+    ],
+    correctAnswer: "To test the code we have written",
   },
 ];
