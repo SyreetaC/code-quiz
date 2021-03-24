4eb34240730ebef0abeef6158e12e5ab51c906f7
Sy C <76875297+SyreetaC@users.noreply.github.com>
2021-03-24 18:28:38 +0000

    added create question function

--- js/script.js
+++ js/script.js
@@ -1,4 +1,40 @@
-const startButtonElement = document.getElementById("start-button");
+const quizQuestions = [
+  {
+    title: "Which HTML element do we put the Javascript inside?",
+    answers: ["js", "script", "javascript"],
+    correctAnswer: "script",
+  },
+
+  {
+    title: "How do you add a comment in Javascript?",
+    answers: ["//", "<!--->", "This is a comment"],
+    correctAnswer: "//",
+  },
+
+  {
+    title: "Which one of these is a Javascript key word?",
+    answers: ["const", "lot", "create", "proof"],
+    correctAnswer: "const",
+  },
+
+  {
+    title: "What statement supplies the value of a function?",
+    answers: ["track", "return", "let", "value"],
+    correctAnswer: "return",
+  },
+
+  {
+    title: "Why do we use the console when coding in Javascript?",
+    answers: [
+      "To make sure everything works",
+      "To make our code look good",
+      "To test the code we have written",
+      "To show the value of variables",
+    ],
+    correctAnswer: "To test the code we have written",
+  },
+];
+
 const bodyElement = document.body;
 const startGameDiv = document.getElementById("start-page-div");
 const timerElement = document.getElementById("time-remaining");
@@ -17,6 +53,9 @@ const countdown = () => {
   };
   const timer = setInterval(timerTick, 1000);
 };
+
+const createQuestion = (question) => {};
+
 //Constructing game elements
 const constructGameContainer = () => {
   const gamePageDiv = document.createElement("div");
@@ -24,6 +63,7 @@ const constructGameContainer = () => {
 
   const gamePageHeader = document.createElement("h4");
   gamePageHeader.setAttribute("class", "question-header");
+  gamePageHeader.textContent = "Question 1";
 
   const gamePageAnswersDiv = document.createElement("div");
   gamePageAnswersDiv.setAttribute("class", "answer-buttons");
@@ -41,13 +81,14 @@ const constructGameContainer = () => {
   return gamePageDiv;
 };
 
+const startButtonElement = document.getElementById("start-button");
 //Start of game
 const startGame = () => {
   console.log("START GAME");
   const gamePageDiv = constructGameContainer();
   bodyElement.removeChild(startGameDiv); //Removes start page
   bodyElement.appendChild(gamePageDiv); //fix CSS for this appended child!
-
+  createQuestion(quizQuestions[0]);
   countdown();
 };
 
