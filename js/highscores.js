a5a3fde3ab4cb2019d2733f88b7e566586e995d3
Sy C <76875297+SyreetaC@users.noreply.github.com>
2021-03-25 14:10:55 +0000

    created highscore page elements

---
+++ js/highscores.js
@@ -0,0 +1,8 @@
+const bodyElement = document.body;
+
+const formContainer = document.createElement("div");
+const form = document.createElement("form");
+const label = document.createElement("label");
+const input = document.createElement("input");
+
+formContainer.appendChild(form, label, input);
