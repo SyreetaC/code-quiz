de10e38272aae7a46630e6f4ec4949c35fc724db
Sy C <76875297+SyreetaC@users.noreply.github.com>
2021-03-27 19:51:33 +0000

    deleted redundant high scores js

--- js/highscores.js
+++
@@ -1,8 +0,0 @@
-const bodyElement = document.body;
-
-const formContainer = document.createElement("div");
-const form = document.createElement("form");
-const label = document.createElement("label");
-const input = document.createElement("input");
-
-formContainer.appendChild(form, label, input);
